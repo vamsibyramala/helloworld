59d810c4cd24fdb8a80f2e075d874dbc442425c5
vamsibyramla <70326727+vamsibyramla@users.noreply.github.com>
2022-08-06 13:53:17 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,6 @@ package hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHelloWorld());
+		System.out.println(greeter.sayHelloWorldJava());
 	}
 }
